2e32f95965af8b9bd0066a7304813453ca9682c9
J-Bills <billsjamal@gmail.com>
2025-06-30 22:46:54 -0700

    Getting the message that the user sent in form and stored in a file and wrote 302 response. Redirected location to home.

--- node-basics/01-routing-requests/app.js
+++ node-basics/01-routing-requests/app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+// importing the module needed to work with the file system
+const fs  = require('fs');
 
 // A way to create a listener but we can also use an arrow function
 // function rqListener(req, res){
@@ -7,13 +9,24 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
   //Parsing the url to start handling routes
   const url = req.url;
+  const method = req.method;
   if(url === '/'){
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method ="POST"><input type="text" name="form-message"><button type="submit">Send</button></form></body>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="form-message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     // return so that we can exit out of the function
     return res.end();
+  }
+  if (url === '/message' && method === "POST"){
+    //TODO: Redirect the user to '/' and create and store the message in a file
+    // Using 302 response and setting location to '/' when that response code is received
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.writeHead(302, {
+      'Location': '/'
+    });
+    return res.end();
+
   }
   res.setHeader('Content-Type', 'text/html');
   //sending html code to the response
