bf56a8881c38569c514c63e3c31226e9cb418d15
J-Bills <billsjamal@gmail.com>
2025-06-30 22:23:15 -0700

    Adding response html to the server

--- node-basics/01-routing-requests/app.js
+++ node-basics/01-routing-requests/app.js
@@ -7,6 +7,13 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
   console.log(req.url, req.method, req.headers);
    // process.exit(); //Hard exits the event loop
+  res.setHeader('Content-Type', 'text/html');
+  //sending html code to the response
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body?><h1> Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
