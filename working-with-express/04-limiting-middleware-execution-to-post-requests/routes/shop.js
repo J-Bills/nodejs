d7e2c9f4ca0489c0c885bc0aa3be6200f3c9d944
J-Bills <billsjamal@gmail.com>
2025-07-02 01:10:30 -0700

    Linked the html pages to the routes by using the path module and created a 404 page for the site in the views folder

--- working-with-express/04-limiting-middleware-execution-to-post-requests/routes/shop.js
+++ working-with-express/04-limiting-middleware-execution-to-post-requests/routes/shop.js
@@ -1,9 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/home', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+    // got an error when trying to run this saying that res.sendFile requires an aboslute path
+    // res.sendFile('../views/shop.html');
+    // so now we have to use the path module to solve this
+    // this joins the paths on the filesystem and makes sure that it works on all os'
+    // we have to add the path piece by piece and since views is a sibling of the routes folder we have to add ../ also
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
