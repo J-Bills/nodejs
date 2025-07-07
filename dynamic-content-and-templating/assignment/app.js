ee02db5e8b7b3f1f31749e98f15f9ad8063aa137
J-Bills <billsjamal@gmail.com>
2025-07-07 16:33:22 -0700

    got the assignment finished but I want to add routes

--- dynamic-content-and-templating/assignment/app.js
+++ dynamic-content-and-templating/assignment/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser');
+
 
 
 const app = express();
@@ -8,22 +9,27 @@ const users = [];
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res, next) => {
-    res.render('index', {pageTitle: 'Add User'});
+    res.render('home', {pageTitle: 'Add User'});
 });
 
 app.get('/users', (req, res, next) => {
-    res.render('users', {pageTitle: 'User'});
+    res.render('users', {
+        pageTitle: 'User',
+        users: users
+    });
 });
 
-app.get('/add-user', (req, res, next) => {
+app.post('/add-user', (req, res, next) => {
+    users.push({name: req.body.username});
     res.redirect('/users');
 });
 
 app.use((req, res, next) => {
     res.status(404).render('404', {
-        PageTitle: 'Page Not Found',
+        pageTitle: 'Page Not Found',
         path: '' // or use req.url if you want to show the attempted path
     });
 });
