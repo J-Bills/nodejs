f37ff8ca89633c67d2d59586a2254a71bbeaec57
J-Bills <billsjamal@gmail.com>
2025-07-07 12:08:30 -0700

    exported data from admin route and got it to our shop route

--- dynamic-content-and-templating/00-starting-setup/routes/admin.js
+++ dynamic-content-and-templating/00-starting-setup/routes/admin.js
@@ -6,6 +6,9 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+// Creating an array to store data into
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -13,8 +16,11 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({title: req.body.title});
   res.redirect('/');
 });
 
-module.exports = router;
+
+module.exports.router = router;
+module.exports.products = products;
+
--- dynamic-content-and-templating/00-starting-setup/routes/shop.js
+++ dynamic-content-and-templating/00-starting-setup/routes/shop.js
@@ -3,10 +3,14 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  // This is data that is shared across all users because it is a part of our node data.
+  // This is kinda a big nono
+  console.log('shop.js', adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
