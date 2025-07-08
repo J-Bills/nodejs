f83fe3cdcf37cd61ce005e134ba4f6c5874b356c
J-Bills <billsjamal@gmail.com>
2025-07-07 18:24:09 -0700

    adding our controllers to our routes]

---
+++ mvc/00-starting-setup/controllers/products.js
@@ -0,0 +1,29 @@
+const products = [];
+//Creating a function to seperate our add-product code and export to link it to our routes
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+    });
+}
+
+exports.postAddProduct = (req, res, next) => {
+    products.push({ title: req.body.title });
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+    //removing products because we have access to it in this controller file
+    // const products = adminData.products;
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+}
