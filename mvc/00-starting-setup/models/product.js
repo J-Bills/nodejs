359edb9371064aae16c2c65d9bc292fec7a9016e
J-Bills <billsjamal@gmail.com>
2025-07-07 19:51:25 -0700

    adding a helper function with a callback to help refactor our code

--- mvc/00-starting-setup/models/product.js
+++ mvc/00-starting-setup/models/product.js
@@ -3,6 +3,18 @@
 
 const fs = require('fs');
 const path = require('path');
+// making the file path global so that we can use the helper function in both of our class methods
+const p = path.join((__dirname), '../data', 'products.json');
+// Creating a helper function to help with grabbing file content
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) =>{
+        if (!err){
+            cb(JSON.parse(fileContent));
+        } else {
+            cb([]);
+        }
+    });
+};
 module.exports = class Product {
     constructor(title){
         this.title = title;
@@ -11,30 +23,18 @@ module.exports = class Product {
         // adding the file path and reading the file that we want to save to
         // if there is no error(data exists in the file) then we parse the content in it
         // then we add out object to the product array and write the file
-        const p = path.join((__dirname), '../data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err){
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
         });
-
     }
     // static makes sure that I can call this method on the class itself and not just the object
     // adding the file path and reading the file that we want to save to
     // if there is no error(data exists in the file) then we parse the content in it and return it
     // else we return an empty array
     static fetchAll(cb){
-        const p = path.join((__dirname), '../data', 'products.json');
-        fs.readFile(p, (err, fileContent) =>{
-            if (!err){
-                cb(JSON.parse(fileContent));
-            }
-            cb([]);
-        });
+        getProductsFromFile(cb);
     }
 };
